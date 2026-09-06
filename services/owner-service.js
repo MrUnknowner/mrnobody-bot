6166be12aae27cc605cbb7bc6dd1170ddd65cda9
MrUnknowner <nobodymdbot@gmail.com>
2026-09-07 00:06:52 +0530

    Update owner-service.js

--- services/owner-service.js
+++ services/owner-service.js
@@ -1,5 +1,5 @@
 const PAIR_API_URL =
-    "https://mrnobody-pair-production-fded.up.railway.app";
+    "https://mrnobody-pair-production-9530.up.railway.app";
 
 function getSessionId() {
     return process.env.SESSION_ID
